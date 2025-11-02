8e73b4d032cc8875f9b182500495e0d4b17bc35d
Elielpg <165718055+Elielpg@users.noreply.github.com>
2025-11-02 12:44:05 -0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,8 +1,18 @@
 @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+setlocal
+
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
+
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-set JAVA_EXE=java
-"%JAVA_EXE%" -Xmx64m -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+@rem Execute Gradle
+"%JAVA_HOME%\bin\java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
