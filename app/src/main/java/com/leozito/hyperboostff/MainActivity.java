30c12511481da16b2aba5a07dea87ae76b9d533b
Elielpg <165718055+Elielpg@users.noreply.github.com>
2025-11-03 13:01:58 -0300

    Update MainActivity.java

--- app/src/main/java/com/leozito/hyperboostff/MainActivity.java
+++ app/src/main/java/com/leozito/hyperboostff/MainActivity.java
@@ -1,31 +1,22 @@
-package com.leozito.hyperboostff;
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
 
-import android.app.Activity;
-import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+    <application
+        android:label="HyperBoost FF"
+        android:allowBackup="true"
+        android:supportsRtl="true"
+        android:icon="@mipmap/logo">
 
-public class MainActivity extends Activity {
-    MediaPlayer mp;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mp = MediaPlayer.create(this, R.raw.boost_sound);
-        Button btn = findViewById(R.id.btn_boost);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Boost ativado!", Toast.LENGTH_SHORT).show();
-                if(mp != null) mp.start();
-            }
-        });
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(mp != null) { mp.release(); mp = null; }
-    }
-}
+        <activity
+            android:name="com.leozito.hyperboostff.MainActivity"
+            android:exported="true">
+
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+
+        </activity>
+
+    </application>
+
+</manifest>
